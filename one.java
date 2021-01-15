3a76a20e5e092be9c0fe3e4b863722c45eb869cb
wdklein <60987248+wdklein@users.noreply.github.com>
2021-01-14 18:40:11 -0600

    Create one.java

---
+++ one.java
@@ -0,0 +1,13 @@
+package java_1;
+
+public class ClassOne {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,2,3};
+		
+		for(int i = 0; i < 3; i ++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
